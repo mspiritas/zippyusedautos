f2810db31626f5c7da5e7b9fd0959f8afbf28907
Monica <m_spiritas@mail.fhsu.edu>
2021-04-18 13:29:07 -0500

    Added VehicleDB

--- model/vehicle_db.php
+++ model/vehicle_db.php
@@ -1,6 +1,7 @@
-<?php 
-    function get_vehicles_by_class($class_id, $sort) {
-        global $db;
+<?php
+class VehicleDB {
+    public static function get_vehicles_by_class($class_id, $sort) {
+        $db = Database::getDB();
         if ($sort == 'year'){
             $orderby = 'V.year';
         } else {
@@ -23,8 +24,8 @@
         return $vehicles;
     }
 
-    function get_vehicles_by_type($type_id, $sort) {
-        global $db;
+    public static function get_vehicles_by_type($type_id, $sort) {
+        $db = Database::getDB();
         if ($sort == 'year'){
             $orderby = 'V.year';
         } else {
@@ -47,8 +48,8 @@
         return $vehicles;
     }
 
-    function get_vehicles_by_make($make_id, $sort) {
-        global $db;
+    public static function get_vehicles_by_make($make_id, $sort) {
+        $db = Database::getDB();
         if ($sort == 'year'){
             $orderby = 'V.year';
         } else {
@@ -71,8 +72,8 @@
         return $vehicles;
     }
 
-    function get_all_vehicles($sort) {
-        global $db;
+    public static function get_all_vehicles($sort) {
+        $db = Database::getDB();
         if ($sort == 'year'){
             $orderby = 'V.year';
         } else {
@@ -91,8 +92,8 @@
         return $vehicles;
     }
 
-    function delete_vehicle($vehicle_id) {
-        global $db;
+    public static function delete_vehicle($vehicle_id) {
+        $db = Database::getDB();
         $query = 'DELETE FROM vehicles WHERE ID = :vehicle_id';
         $statement = $db->prepare($query);
         $statement->bindValue(':vehicle_id', $vehicle_id);
@@ -100,8 +101,8 @@
         $statement->closeCursor();
     }
 
-    function add_vehicle($make_id, $type_id, $class_id, $year, $model, $price) {
-        global $db;
+    public static function add_vehicle($make_id, $type_id, $class_id, $year, $model, $price) {
+        $db = Database::getDB();
         $query = 'INSERT INTO vehicles (year, make_id, model, price, type_id, class_id)
               VALUES
                  (:year, :make_id, :model, :price, :type_id, :class_id)';
@@ -115,3 +116,4 @@
         $statement->execute();
         $statement->closeCursor();
     }
+}
